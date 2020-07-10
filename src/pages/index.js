80090436916f534f5a3b81ddecf1cab946d2c8ab
andydlin <andydlin@gmail.com>
2020-07-10 14:06:01 -0700

    netlify is showing that projectsList can't be resolved

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ import { colors } from '../styles/colors'
 import { Box, Container } from '../styles/global'
 import { spacing } from '../styles/spacing'
 import { H1, H2, H3, H4 } from "../styles/typography"
-import ProjectList from "../components/projectsList"
+import ProjectList from "../components/projectsList.js"
 
 const GreetingMessage = styled.h1`
   color: ${colors.gray200};
