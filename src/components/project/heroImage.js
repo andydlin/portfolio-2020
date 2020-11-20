2d2dc041e6f13bb1fcf9ff1adf81958bf1c999b5
David Sally <davidcsally@gmail.com>
2020-11-19 23:37:09 -0800

    Cleanup heroImage
    I would pass the explicit prop and use styled(Img) over css.

--- src/components/project/heroImage.js
+++ src/components/project/heroImage.js
@@ -1,17 +1,12 @@
 import React from "react"
 import Img from "gatsby-image"
+import styled from "styled-components"
 
-const HeroImage = (props) => {
+const StyledImg = styled(Img)`
+  height: calc(100vh - 124px);
+  width: 100vw;
+`
 
-  return (
-    <Img
-      fluid={props.image}
-      css={`
-        height: calc(100vh - 124px);
-        width: 100vw;
-      `}
-    />
-  )
-}
+const HeroImage = ({ image }) => <StyledImg fluid={image} />
 
 export default HeroImage
